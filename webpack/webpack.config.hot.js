1df814b5e455c1c1952a7ab104d919f2ac88bc5e
Kaka <hlt1988321@163.com>
2017-02-25 11:34:39 +0800

    Beautify code & Add build mode
    Build mode:
    - hot: startup webpack-dev-server with react-hot-loader
    - dev: bundle code then emit JS and CSS with watch code be changed.
    - build: bundle code then emit JS and CSS.

---
+++ webpack/webpack.config.hot.js
@@ -0,0 +1,98 @@
+const webpack = require('webpack')
+// const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+const rootDir = path.resolve(__dirname, '..')
+const devServerPort = 5000
+
+module.exports = {
+  entry: {
+    app: [
+      `webpack-dev-server/client?http://0.0.0.0:${devServerPort}`,
+      'webpack/hot/only-dev-server',
+      path.resolve(rootDir, 'src/index'),
+    ],
+  },
+
+  output: {
+    path: path.resolve(rootDir, 'dist'),
+    filename: '[name].js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: [
+          'react-hot',
+          'babel',
+        ],
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg)$/,
+        loader: 'url',
+        query: {
+          limit: 0,
+          name: '/font/[name].[ext]',
+        },
+      },
+      {
+        test: /\.(png|gif|jpe?g)$/,
+        loader: 'url',
+        query: {
+          limit: 0,
+          name: '/img/[name].[ext]',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('dev'),
+      },
+    }),
+
+    // Enable multi-pass compilation for enhanced performance in larger projects
+    new webpack.HotModuleReplacementPlugin({
+      multiStep: true,
+    }),
+
+    // new ExtractTextPlugin('[name].css', { allChunks: true }),
+
+    new HtmlPlugin({
+      title: 'Dev-index',
+      filename: 'index.html',
+      template: path.resolve(rootDir, './dev/tpl/index.ejs'),
+    }),
+  ],
+
+  devServer: {
+    historyApiFallback: false,
+
+    // Unlike the cli flag, this doesn't set HotModuleReplacementPlugin!
+    hot: true,
+
+    // If hot loader fail, inline use to notify browser refresh
+    inline: true,
+    host: '0.0.0.0',
+    port: devServerPort,
+    // proxy: {
+    //   '**': {
+    //     target: 'http://localhost:4000',
+    //     secure: false
+    //   }
+    // }
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css', '.scss'],
+  },
+}
--- webpack/webpack.config.release.js
+++ webpack/webpack.config.release.js
@@ -4,38 +4,35 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
 
 const rootDir = path.resolve(__dirname, '..')
+const copyRight = 'copy right @ Kaka'
 
 module.exports = {
   entry: {
-    app: [
-      path.resolve(rootDir, 'src/index'),
-    ],
+    app: path.resolve(rootDir, 'src/index'),
   },
 
   output: {
     path: path.resolve(rootDir, 'dist'),
-    filename: '[name].js',
+    filename: '[name].min.js',
   },
 
   module: {
     loaders: [
       {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', ['css?sourceMap', 'sass?sourceMap']),
-      },
-      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: [
-          'babel',
-        ],
+        loader: 'babel',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', ['css?sourceMap', 'sass?sourceMap']),
       },
       {
         test: /\.(eot|woff|woff2|ttf|svg)$/,
         loader: 'url',
         query: {
           limit: 1,
-          name: '/font/[name].[ext]',
+          name: 'font/[name].[ext]',
         },
       },
       {
@@ -43,22 +40,30 @@ module.exports = {
         loader: 'url',
         query: {
           limit: 1,
-          name: '/img/[name].[ext]',
+          name: 'img/[name].[ext]',
         },
       },
     ],
   },
 
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('dev'),
-      },
-    }),
-
     new CleanPlugin(['dist'], {
       root: rootDir,
     }),
+
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+
+    new webpack.BannerPlugin(copyRight),
 
     new ExtractTextPlugin('[name].css', { allChunks: true }),
   ],
